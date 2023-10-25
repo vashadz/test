26136d7c8d1e622b8bd1570edf0e2bf2bd313fa2
Vasiliki Hadzikosta <65341146+vashadz@users.noreply.github.com>
2023-10-24 22:42:43 -0400

    Add files via upload

---
+++ vasiliki/sketch.js
@@ -0,0 +1,148 @@
+// an array for the magnitude
+var magnitudes;
+// an array for depth
+var depths;
+// an array for lat & long
+var latitudes, longitudes;
+
+// minimum and maximum values for magnitude and depth
+var magnitudeMin, magnitudeMax;
+var depthMin, depthMax;
+
+// the dots we'll be adding to the map
+var circles = [];
+
+// table as the data set
+var table;
+
+// my leaflet.js map
+var mymap;
+
+function preload() {
+    // load the CSV data into our `table` variable and clip out the header row
+    table = loadTable("assets/all_month.csv", "csv", "header");
+}
+
+function setup() {
+    /*
+    P5 SETUP
+
+    If you want to draw some diagrams to complement the map view, set up your canvas
+    size, color, etc. here
+    */
+    createCanvas(100, 100);
+    background(200);
+
+    // on the left, draw a gradient with 9 shades drawn from the red sequential palette
+    var pal = Brewer.sequential('Reds', 9, 0, 100) // (paletteName, numColors, minValue, maxValue)
+    console.log('sequential palette:', pal.colors)
+    for (var i=0; i<100; i++){
+        noStroke()
+        fill(pal.colorForValue(i))
+        rect(0,i, width/2,i+1)
+    }
+
+    // on the right, draw a gradient with 'infinite' (i.e., interpolated) shades drawn from
+    // the red-yellow-green divergent palette
+    var pal2 = Brewer.divergent('RdYlGn', Infinity, 0, 33, 100) // (paletteName, numColors, minValue, midpoint, maxValue)
+    console.log('divergent palette:', pal2.colors)
+    for (var i=0; i<100; i++){
+        noStroke()
+        fill(pal2.colorForValue(i))
+        rect(width/2,i, width,i+1)
+    }
+
+    // happy little snowman
+    textSize(64);
+    fill(255)
+    text("☃", 18, 72);
+
+
+    /*
+    LEAFLET CODE
+
+    In this case "L" is leaflet. So whenever you want to interact with the leaflet library
+    you have to refer to L first.
+    so for example L.map('mapid') or L.circle([lat, long])
+    */
+
+    // create your own map
+    mymap = L.map('quake-map').setView([51.505, -0.09], 3);
+
+    // load a set of map tiles – choose from the different providers demoed here:
+    // https://leaflet-extras.github.io/leaflet-providers/preview/
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 18,
+    }).addTo(mymap);
+
+    // call our function (defined below) that populates the maps with markers based on the table contents
+    drawDataPoints();
+}
+
+function drawDataPoints(){
+    strokeWeight(5);
+    stroke(255,0,0);
+
+    // get the two arrays of interest: depth and magnitude
+    depths = table.getColumn("depth");
+    magnitudes = table.getColumn("mag");
+    latitudes = table.getColumn("latitude");
+    longitudes = table.getColumn("longitude");
+
+    // get minimum and maximum values for both
+    magnitudeMin = 0.0;
+    magnitudeMax = getColumnMax("mag");
+    console.log('magnitude range:', [magnitudeMin, magnitudeMax])
+
+    depthMin = 0.0;
+    depthMax = getColumnMax("depth");
+    console.log('depth range:', [depthMin, depthMax])
+
+    // cycle through the parallel arrays and add a dot for each event
+    for(var i=0; i<depths.length; i++){
+        // create a new dot
+        var circle = L.circle([latitudes[i], longitudes[i]], {
+            color: 'red',      // the dot stroke color
+            fillColor: '#f03', // the dot fill color
+            fillOpacity: 0.25,  // use some transparency so we can see overlaps
+            radius: magnitudes[i] * 40000
+        });
+
+        // place it on the map
+        circle.addTo(mymap);
+
+        // save a reference to the circle for later
+        circles.push(circle)
+    }
+}
+
+function removeAllCircles(){
+    // remove each circle from the map and empty our array of references
+    circles.forEach(function(circle, i){
+        mymap.removeLayer(circle);
+    })
+    circles = [];
+}
+
+// get the maximum value within a column
+function getColumnMax(columnName){
+    // get the array of strings in the specified column
+    var colStrings = table.getColumn(columnName);
+
+    // convert to a list of numbers by running each element through the `float` function
+    var colValues = _.map(colStrings, float);
+
+    // find the max value by manually stepping through the list and replacing `m` each time we
+    // encounter a value larger than the biggest we've seen so far
+    var m = 0.0;
+    for(var i=0; i<colValues.length; i++){
+        if (colValues[i] > m){
+            m = colValues[i];
+        }
+    }
+    return m;
+
+    // or do it the 'easy way' by using lodash:
+    // return _.max(colValues);
+}
